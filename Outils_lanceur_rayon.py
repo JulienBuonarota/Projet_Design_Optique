eab783e71f35973d60d9f4b659f193d97265c0f8
Julien Buonarota <j.buohk@gmail.com>
2021-05-07 20:56:59 +0200

    implementation stop algo refraction quand pas de convergence

--- Outils_lanceur_rayon.py
+++ Outils_lanceur_rayon.py
@@ -1,9 +1,50 @@
 import numpy as np
 import Object_rayon as oray
 
-def intersection():
-    # TODO fct calcul pts d'intersection entre un rayon et une surface
-    pass
+def intersection(P0, C0, O0, R, F, Fp, normal, n0, n1):
+    """
+    Calcul de l'intersection en un rayon et une surface
+
+    :param P0: [X0, Y0, Z0], point d'origine du rayon lumineux incident
+    :param C0: Vecteur [k0, l0, m0], vecteur unitaire de direction du rayon lumineux incident
+    :param O0: [x0, y0, z0], origine du repère de la surface exprimé dans le repère de référence
+    :param R: Matrice de rotation de passage du repère de référence au repère de la surface de réfraction
+    :param F: Fonction algébrique du dioptre (F(x, y, z) = 0)
+    :param Fp: Fp = (dF/dx)(P)*k + (dF/dy)(P)*l + (dF/dz)(P)*m
+    :return: Pi0
+    """
+    ## Changement de repère du rayon
+    #  Calcul de l'origine et de la direction dans le nouveau repère
+    P1 = np.matmul(R, P0 - O0)
+    C1 = np.matmul(R, C0)
+
+    ## Find the point of intersection of the ray with the surface S.
+    # calcul de l'intersection du rayon avec le plan z0 = 0 du nouveau repère
+    # equation parametric du rayon sont : X = X1 + k1*s, Y = Y1 + l1*s, Z = Z1 + m1*s
+    P1s0 = P1 - P1[2] / C1[2] * C1
+    # Calcul de l'intersection par algo de Newton-Raphson
+    count = 0
+    nb_iter_max = 1000
+    s = 0
+    while True:
+        P = P1s0 + C1*s
+        sn = s - F(P)/Fp(P, C1)
+        if np.abs(s - sn) < 1e-3:
+            break
+        s = sn
+        # evite la recherche infini
+        if count > nb_iter_max:
+            raise "NoIntersection"
+        count += 1    
+
+    # parametre du chemin origine -> intersection
+    p = -P1[2]/C1[2] + sn
+    # coordonnées de l'intersection
+    Pi = P1 + C1*p
+    
+    return Pi
+
+
 
 def reflexion():
     # TODO fct de reflexion
@@ -32,6 +73,8 @@ def refraction(P0, C0, O0, R, F, Fp, normal, n0, n1):
     # equation parametric du rayon sont : X = X1 + k1*s, Y = Y1 + l1*s, Z = Z1 + m1*s
     P1s0 = P1 - P1[2] / C1[2] * C1
     # Calcul de l'intersection par algo de Newton-Raphson
+    count = 0
+    nb_iter_max = 1000
     s = 0
     while True:
         P = P1s0 + C1*s
@@ -39,6 +82,10 @@ def refraction(P0, C0, O0, R, F, Fp, normal, n0, n1):
         if np.abs(s - sn) < 1e-3:
             break
         s = sn
+        # evite la recherche infini
+        if count > nb_iter_max:
+            raise "NoIntersection"
+        count += 1    
 
     # parametre du chemin origine -> intersection
     p = -P1[2]/C1[2] + sn
@@ -53,13 +100,18 @@ def refraction(P0, C0, O0, R, F, Fp, normal, n0, n1):
     b = ((n0/n1)**2 - 1)/np.sum(r*r)
 
     # Calcul de la solution de l'equation par algo de Newton-Raphson
+    count = 0
+    nb_iter_max = 1000
     T = -b/(2*a)
     while True:
         Tn = (T**2 - b)/(2*(T + a))
         if np.abs(T - Tn) < 1e-3:
             break
         T = Tn
-
+    if count > nb_iter_max:
+        raise "NoSolution"
+    count += 1
+    
     # Unit direction vector of refracted ray
     Cp1 = n0/n1*C1 + T*r
 
